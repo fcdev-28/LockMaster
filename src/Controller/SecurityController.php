e2f4bcd8c12bb6af41ed5a1dfe8a76aa3d10957c
Francisco Cañera <247251387+fcdev-28@users.noreply.github.com>
2026-09-01 18:42:06 +0200

    Add 'index' route alias so base.html.twig can resolve path('index')

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,6 +30,9 @@ class SecurityController extends AbstractController
         private EntityManagerInterface $entityManager
     ) {}
 
+    // 'index' es el nombre que usan base.html.twig y el resto de plantillas;
+    // 'landing_page' es el que usa forms/login.html.twig. Mantenemos los dos.
+    #[Route('/', name: 'index')]
     #[Route('/', name: 'landing_page')]
     public function landingPage(): Response
     {
